dfbe8b82c3bf6ae7c344267dfa861730f085f545
Anastasia <shvayka92@mail.ru>
2022-08-14 14:49:10 +0300

    Initial commit

---
+++ src/main/java/com/example/hw5_collections/Hw5CollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hw5_collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw5CollectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw5CollectionsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/hw5_collections/controller/Controller.java
@@ -0,0 +1,2 @@
+package com.example.hw5_collections.controller;public class Controller {
+}
